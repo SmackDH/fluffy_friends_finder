51ca8d031d25997a6b5a0a5927d3f25ac058b1e8
Yumi Kimura <kimurayumixy@gmail.com>
2022-11-15 15:00:29 +0900

    I think my booking controller is done

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-<<<<<<< HEAD
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  resources :pets do
-    resources :bookings, only: %i[new create]
-=======
   root to: 'pages#home'
   resources :pets, only: [:index, :show, :new, :create] do
     resources :bookings, only: :create
@@ -16,6 +7,5 @@ Rails.application.routes.draw do
   resources :bookings, only: [:index, :update]
   namespace :owner do
     resources :bookings, only: :index
->>>>>>> 005412a2fbfcf65645b5a0c5bcf9a42929271964
   end
 end
--- db/migrate/20221115030543_create_bookings.rb
+++ db/migrate/20221115030543_create_bookings.rb
@@ -8,6 +8,8 @@ class CreateBookings < ActiveRecord::Migration[7.0]
       t.date :date_end
 
       t.timestamps
+      validates :date_start, presence: true
+      validates :date_start, presence: true
       has_many :pets
       has_many :users
       validates :pets, presence: true, uniqueness: { scope: :users }
