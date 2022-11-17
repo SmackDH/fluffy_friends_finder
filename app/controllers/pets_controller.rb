c1536ac8ef93f650edcde4753a7f3bd45fe61838
Mattias Welamsson <mattias@iMac.local>
2022-11-17 13:03:19 +0900

    search added to navbar

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,7 +6,11 @@ class PetsController < ApplicationController
   end
 
   def index
-    @pets = policy_scope(Pet)
+    if params[:query].present?
+      @pets = policy_scope(Pet).where(description: params[:query])
+    else
+      @pets = policy_scope(Pet)
+    end
   end
 
   def show
