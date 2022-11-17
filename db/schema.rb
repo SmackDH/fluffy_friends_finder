4d4034e385bb83431a78671d81b58f61f0c41ea4
Soren Umstot <soren.umstot@gmail.com>
2022-11-17 15:09:33 +0900

    add a new pet functionality working. User is able to add a pet with images, set the price.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_17_052631) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_17_055219) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -56,7 +56,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_17_052631) do
 
   create_table "pets", force: :cascade do |t|
     t.string "name"
-    t.boolean "available"
+    t.boolean "available", default: true
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
