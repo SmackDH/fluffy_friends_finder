624d8187e6ef54a9af0652467a53754df4c09b5a
Yumi Kimura <kimurayumixy@gmail.com>
2022-11-17 15:29:42 +0900

    owner index done

--- app/policies/owner/booking_policy.rb
+++ app/policies/owner/booking_policy.rb
@@ -1,18 +1,25 @@
 class Owner::BookingPolicy < ApplicationPolicy
   class Scope < Scope
-
-    def create?
-      record.user == user
+    def resolve
+      scope.joins(:pet).where(pets: {user: user})
     end
 
-    def update?
-      record.user == user
-      # record: the restaurant passed to the `authorize` method in controller
-      # user: the `current_user` signed in with Devise
+
+    def index?
+      true
     end
 
-    def destroy?
-      record.user == user
-    end
+  end
+  def update?
+    record.user == user
+    # record: the restaurant passed to the `authorize` method in controller
+    # user: the `current_user` signed in with Devise
+  end
+
+  def destroy?
+    record.user == user
+  end
+  def create?
+    record.user == user
   end
 end
