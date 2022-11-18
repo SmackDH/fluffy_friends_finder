b9cadfee5abb9fda4d0978000626146a30ad2bda
Mattias Velamsson <mattiaswelamsson@gmail.com>
2022-11-18 16:25:03 +0900

    added logic for has_pets or not into navbar links.

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -7,8 +7,8 @@ class Pet < ApplicationRecord
   include PgSearch::Model
 
   # pg_search_scope :date_between, against: [ :start, :end ]
-  pg_search_scope :global_search, against: [ :name, :species ],
-   associated_against: {user: [ :username] #=> Change this to "user's name"-key when added.
+  pg_search_scope :global_search, against: [ :name, :species, :city],
+   associated_against: {user: [ :name] #=> Change this to "user's name"-key when added.
     },
      using: {
       tsearch: { prefix: true }
